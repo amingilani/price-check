3cd348608ccdc9cf60c9fbadc49ea02956f52518
amingilani <amin@gilani.me>
2017-02-05 03:59:59 +0500

    Made the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            price: null
+        }
+    }
+
+    checkPrice() {
+        axios.get('http://api.coindesk.com/v1/bpi/currentprice/usd.json').then(function(res) {
+          window.hello = res;
+            const rate = res.data.bpi.USD.rate
+            console.log(rate)
+            this.setState({price: rate});
+        }.bind(this))
+    }
+    render() {
+        const price = this.state.price
+        if (price === null)
+            this.checkPrice();
+        return (
+            <div className="App">
+                <p>
+                    the current price of a bitcoin is ${price
+                        ? price
+                        : 'loading...'}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
